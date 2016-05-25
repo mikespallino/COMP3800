b3fd6442722153d1240b40beb6e25fa1955534a1
Michael Spallino <mikehspallino@gmail.com>
2016-05-25 09:42:25 -0400

    [WEBSERVER] - Make config values get urlencoded

--- SpotifyAPI/web_server.py
+++ SpotifyAPI/web_server.py
@@ -1,13 +1,14 @@
 import flask
 import configparser
+import urllib.parse
 
 config = configparser.ConfigParser()
 config.read('/sapiws/.conf')
 
 CLIENT_ID = config['API_SETTINGS']['API_ID']
 CLIENT_SECRET = config['API_SETTINGS']['API_SECRET']
-SCOPES = config['API_SETTINGS']['SCOPE']
-WEBSERVER_URL = config['WEBSERVER_SETTINGS']['URL']
+SCOPES = urllib.parse.quote_plus(config['API_SETTINGS']['SCOPE'])
+WEBSERVER_URL = urllib.parse.quote_plus(config['WEBSERVER_SETTINGS']['URL'])
 AUTHORIZE_URL = 'https://accounts.spotify.com/authorize?{params}'
 AUTHORIZE_PARAMS = 'response_type=code&client_id={cid}&scopes={scopes}&redirect_uri={redirect}'
 app = flask.Flask('SAPI Web Server')
