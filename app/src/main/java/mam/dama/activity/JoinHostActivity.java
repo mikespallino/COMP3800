6089a915345249090701c7b43a91b14aaf56dbfb
Michael Spallino <mikehspallino@gmail.com>
2016-06-09 08:27:02 -0400

    [MOBILE APP] - Added host & join activty - Added color for buttons

---
+++ app/src/main/java/mam/dama/activity/JoinHostActivity.java
@@ -0,0 +1,15 @@
+package mam.dama.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mam.dama.R;
+
+public class JoinHostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_host);
+    }
+}
