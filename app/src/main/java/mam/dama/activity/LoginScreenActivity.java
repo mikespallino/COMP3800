3fedb9bda5713d52fca4e8a190bf1bffbcd150da
Michael Spallino <mikehspallino@gmail.com>
2016-06-10 08:12:18 -0400

    [MOBILE APP] - Added host playlist picker

--- app/src/main/java/mam/dama/activity/LoginScreenActivity.java
+++ app/src/main/java/mam/dama/activity/LoginScreenActivity.java
@@ -1,7 +1,10 @@
 package mam.dama.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import mam.dama.R;
 
@@ -11,5 +14,15 @@ public class LoginScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        // TODO: Actually connect later.
+        Button loginButton = (Button) findViewById(R.id.button_connect);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent login = new Intent(LoginScreenActivity.this, JoinHostActivity.class);
+                startActivity(login);
+            }
+        });
     }
 }
