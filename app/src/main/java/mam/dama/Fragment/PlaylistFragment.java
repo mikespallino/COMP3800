5cb7ab3b0564f11340e2fe4733682b33febbbc56
Andy Tse <tsea@wit.edu>
2016-06-10 13:55:04 -0400

    [MOBILE APP] -Working HubActivity and Fragments

--- app/src/main/java/mam/dama/Fragment/PlaylistFragment.java
+++ app/src/main/java/mam/dama/Fragment/PlaylistFragment.java
@@ -1,5 +1,6 @@
 package mam.dama.Fragment;
 
+import android.app.FragmentManager;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -7,68 +8,56 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import mam.dama.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link PlaylistFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- *
- */
 public class PlaylistFragment extends Fragment {
 
-    private OnFragmentInteractionListener mListener;
+    private Button addRequest;
+    private Button viewRequest;
+    private View rootView;
 
     public PlaylistFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_playlist, container, false);
-    }
+        rootView = inflater.inflate(R.layout.fragment_playlist, container, false);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+        addRequest = (Button)(rootView.findViewById(R.id.button_addRequest));
+        viewRequest = (Button)(rootView.findViewById(R.id.button_viewRequest));
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
+        addRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle request_add = new Bundle();
+                FragmentManager fm = getFragmentManager();
+                Fragment fragment = new RequestAddFragment();
+                fragment.setArguments(request_add);
+                fm.beginTransaction().replace(R.id.content_hub,fragment).addToBackStack(null).commit();
+            }
+        });
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
+        viewRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle request_view = new Bundle();
+                FragmentManager fm = getFragmentManager();
+                Fragment fragment = new RequestViewFragment();
+                fragment.setArguments(request_view);
+                fm.beginTransaction().replace(R.id.content_hub,fragment).addToBackStack(null).commit();
+            }
+        });
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        return rootView;
     }
 }
