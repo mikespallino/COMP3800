024b795668ff83af77395a4aaf201c4f2971adc8
Michael Spallino <mikehspallino@gmail.com>
2016-07-25 17:44:42 -0400

    [WEBSERVER] - Added webserver. - Added host event creation through web server.

--- app/src/main/java/mam/dama/activity/HostPlaylistPickerActivity.java
+++ app/src/main/java/mam/dama/activity/HostPlaylistPickerActivity.java
@@ -5,11 +5,12 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.v4.app.INotificationSideChannel;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.SearchView;
 import android.text.InputType;
 import android.util.Log;
 import android.view.View;
@@ -18,16 +19,29 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.SearchView;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.lang.reflect.Array;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Map;
+
+import org.json.*;
 
 import mam.dama.R;
 
 public class HostPlaylistPickerActivity extends AppCompatActivity {
-
     private String eventNameText = "";
     private String eventNamePassword = "";
 
@@ -107,6 +121,7 @@ public class HostPlaylistPickerActivity extends AppCompatActivity {
                 final String selectedPlaylist = (String) playlistView.getItemAtPosition(position);
                 final long selectedPlaylistId = playlistIDMap.get(selectedPlaylist);
                 final ArrayList<String> playlistSongs = new ArrayList<>();
+                final ArrayList<String> allSongs = new ArrayList<String>();
 
                 // The following sets up the Alert Dialog to create a new event.
                 AlertDialog.Builder builder = new AlertDialog.Builder(HostPlaylistPickerActivity.this);
@@ -134,6 +149,8 @@ public class HostPlaylistPickerActivity extends AppCompatActivity {
                     public void onClick(DialogInterface dialog, int which) {
                         // Get all of the tracks for the selected playlist
                         final Uri uri = MediaStore.Audio.Playlists.Members.getContentUri("external", selectedPlaylistId);
+                        final Uri all_uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                        Cursor all_media = resolver.query(all_uri, null, MediaStore.Audio.Media.IS_MUSIC + "!= 0", null, null);
                         Cursor tracks = resolver.query(uri, new String[] {"*"}, null, null, null);
 
                         if(tracks != null) {
@@ -144,6 +161,13 @@ public class HostPlaylistPickerActivity extends AppCompatActivity {
                                 playlistSongs.add(trackName);
                             }
                             tracks.close();
+                        }
+
+                        if(all_media != null) {
+                            for (int i = 0; i < all_media.getCount(); i++) {
+                                all_media.moveToPosition(i);
+                                allSongs.add(all_media.getString(all_media.getColumnIndex(MediaStore.Audio.Media.DATA)));
+                            }
                         }
 
                         // Log important stuff and switch to the hub activity
@@ -158,6 +182,10 @@ public class HostPlaylistPickerActivity extends AppCompatActivity {
                         hubBundle.putString("playlist_name", selectedPlaylist);
                         hubBundle.putStringArrayList("playlist_songs", playlistSongs);
                         hubIntent.putExtras(hubBundle);
+
+                        CreateEvent createEvent = new CreateEvent(eventNameText, eventNamePassword, selectedPlaylist, playlistSongs, allSongs);
+                        createEvent.execute();
+
                         startActivity(hubIntent);
                     }
                 });
@@ -171,6 +199,87 @@ public class HostPlaylistPickerActivity extends AppCompatActivity {
                 builder.show();
             }
         });
+    }
+
+    class CreateEvent extends AsyncTask<String, Void, String> {
+
+        private String eventNameText, eventNamePassword, selectedPlaylist;
+        private ArrayList<String> playlistSongs, allSongs;
+
+        public CreateEvent(String eventName, String eventPassword, String selectedPlaylist, ArrayList<String> playlistSongs, ArrayList<String> allSongs){
+            super();
+            this.eventNameText = eventName;
+            this.eventNamePassword = eventPassword;
+            this.selectedPlaylist = selectedPlaylist;
+            this.playlistSongs = playlistSongs;
+            this.allSongs = allSongs;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            JSONObject hostData = new JSONObject();
+            try {
+                hostData.put("event_name", eventNameText);
+                if (eventNamePassword == "") {
+                    hostData.put("event_password", JSONObject.NULL);
+                } else {
+                    hostData.put("event_password", eventNamePassword);
+                }
+                hostData.put("key", "DAMA");
+                hostData.put("location", "Boston");
+                hostData.put("playlist_name", selectedPlaylist);
+                hostData.put("playlist_songs", new JSONArray(playlistSongs));
+                hostData.put("all_songs", new JSONArray(allSongs));
+            } catch (org.json.JSONException e) {
+                Log.v("DAMA", "SHIT BROKE YO.");
+            }
+
+            HttpURLConnection conn = null;
+            try {
+                URL url = new URL("http://192.241.149.243:8080/host");
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+                conn.setRequestProperty("Accept", "application/json");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+                conn.setUseCaches(false);
+                conn.setInstanceFollowRedirects(true);
+                conn.connect();
+
+                OutputStream sendData = conn.getOutputStream();
+                OutputStreamWriter streamWriter = new OutputStreamWriter(sendData, "UTF-8");
+                streamWriter.write(hostData.toString());
+                streamWriter.flush();
+                streamWriter.close();
+
+                Log.v("DAMA", hostData.toString());
+
+                InputStream in = new BufferedInputStream(conn.getInputStream());
+
+                StringBuffer sb = new StringBuffer();
+                BufferedReader br = new BufferedReader(new InputStreamReader(in));
+                String read;
+
+                while((read= br.readLine())!=null)
+                {
+                    sb.append(read);
+
+                }
+                br.close();
+                Log.v("DAMA-POST", sb.toString());
+
+                //try {Thread.sleep(5000); } catch (InterruptedException e) {}
+
+                conn.disconnect();
+
+            }catch (Exception ex) {
+                Log.e("DAMA", ex.toString());
+                Log.e("DAMA", ex.getLocalizedMessage());
+                return "Error";
+            }
+            return "Done";
+        }
     }
 
 }
---
+++ webserver/queries.py
@@ -0,0 +1,14 @@
+CREATE_EVENT = """
+               INSERT INTO DAMA.events (event_name, event_password, playlist_name, playlist_songs, all_songs, location)
+               VALUES (%s,%s,%s,%s,%s,%s);
+               """
+DISCOVER_EVENT = """
+                 SELECT event_name FROM DAMA.events WHERE location = (%s);
+                 """
+DELETE_EVENT = """
+               DELETE FROM DAMA.events WHERE event_name = ("{event_name}") AND event_password {event_password};
+               """
+JOIN_EVENT = """
+             SELECT playlist_name, playlist_songs, all_songs FROM DAMA.events
+             WHERE event_name = ("{event_name}") AND event_password {event_password};
+             """
