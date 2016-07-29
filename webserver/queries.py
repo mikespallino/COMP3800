47b767cc352dcaf5e4ad71c6df8f5d459b74cb5e
Michael Spallino <mikehspallino@gmail.com>
2016-07-29 11:02:58 -0400

    [FEATURE] - Get events by uuid - Get and Set cur_play

--- webserver/queries.py
+++ webserver/queries.py
@@ -1,6 +1,6 @@
 CREATE_EVENT = """
-               INSERT INTO DAMA.events (event_name, event_password, playlist_name, playlist_songs, all_songs, location, playlist_id)
-               VALUES (%s,%s,%s,%s,%s,%s,%s);
+               INSERT INTO DAMA.events (event_uuid, event_name, event_password, playlist_name, playlist_songs, all_songs, location, cur_play)
+               VALUES (%s,%s,%s,%s,%s,%s,%s,%s);
                """
 DISCOVER_EVENT = """
                  SELECT event_name FROM DAMA.events WHERE location = (%s);
@@ -12,3 +12,9 @@ JOIN_EVENT = """
              SELECT playlist_name, playlist_songs, all_songs FROM DAMA.events
              WHERE event_name = ("{event_name}") AND event_password {event_password};
              """
+SET_CUR_PLAY = """
+               UPDATE DAMA.events SET cur_play=%s WHERE event_uuid=%s;
+               """
+GET_CUR_PLAY = """
+               SELECT cur_play FROM DAMA.events WHERE event_uuid=%s;
+               """
