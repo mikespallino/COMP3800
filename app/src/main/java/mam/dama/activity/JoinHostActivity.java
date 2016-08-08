61037e45f7e1dfc87f9e73a84a1b583e1702138f
Michael Spallino <mikehspallino@gmail.com>
2016-08-08 12:52:09 -0400

    [BUGFIX] - Make sure the buttons actually work...

--- app/src/main/java/mam/dama/activity/JoinHostActivity.java
+++ app/src/main/java/mam/dama/activity/JoinHostActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -27,12 +28,13 @@ public class JoinHostActivity extends AppCompatActivity {
 
             if (ActivityCompat.shouldShowRequestPermissionRationale(JoinHostActivity.this,
                     Manifest.permission.READ_EXTERNAL_STORAGE)) {
-
             } else {
                 ActivityCompat.requestPermissions(JoinHostActivity.this,
                         new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
                         PERMISSIONS_REQUEST_READ_STORAGE);
             }
+        } else {
+            setUpButtons();
         }
     }
 
@@ -43,32 +45,34 @@ public class JoinHostActivity extends AppCompatActivity {
             case PERMISSIONS_REQUEST_READ_STORAGE: {
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // Move to the Host Playlist Picker activity when the host button is clicked
-                    Button hostButton = (Button) findViewById(R.id.button_host);
-                    hostButton.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            Intent intent = new Intent(JoinHostActivity.this, HostPlaylistPickerActivity.class);
-                            startActivity(intent);
-                            finish();
-                        }
-                    });
-                    // Move to the Join Event Picker Activity when the join button is clicked
-                    Button joinButton = (Button) findViewById(R.id.button_join);
-                    joinButton.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            Intent intent = new Intent(JoinHostActivity.this, JoinEventPickerActivity.class);
-                            startActivity(intent);
-                        }
-                    });
-
+                    setUpButtons();
                 } else {
                     finish();
                 }
                 return;
             }
         }
+    }
+
+    private void setUpButtons() {
+        // Move to the Host Playlist Picker activity when the host button is clicked
+        Button hostButton = (Button) findViewById(R.id.button_host);
+        hostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(JoinHostActivity.this, HostPlaylistPickerActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        // Move to the Join Event Picker Activity when the join button is clicked
+        Button joinButton = (Button) findViewById(R.id.button_join);
+        joinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(JoinHostActivity.this, JoinEventPickerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
